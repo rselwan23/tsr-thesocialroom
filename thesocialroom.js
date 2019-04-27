08c4e24718c540a24dff086ca3900c88044f2639
Roger Selwan <roger.eselwan@gmail.com>
2019-04-27 19:45:46 +0300

    first draft

---
+++ thesocialroom.js
@@ -0,0 +1,9 @@
+function validation(){
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username == 'test123')
+    {
+        window.location = "test.html";
+    }
+}
